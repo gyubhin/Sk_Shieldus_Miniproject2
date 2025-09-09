cba981a161fb3bd6ffadd124e43d576325b7aa68
yunjongyun <yunyami0605@gmail.com>
2025-09-09 10:09:08 +0900

    design: 검색, 메인 페이지 ui 추가
    1. 메인, 검색 페이지 ui 추가
    2. 라우터 추가
    3. FilterList 컴포넌트 ui 추가
    4. LoginPage ui 수정

--- front/src/pages/login/LoginPage.tsx
+++ front/src/pages/login/LoginPage.tsx
@@ -1,12 +1,3 @@
-import { ActiveButton } from "@/shared/components/button/ActiveButton";
-import { DropdownButton } from "@/shared/components/dropdown/DropdownButton";
-import { InputField } from "@/shared/components/input/InputField";
-import { SearchInput } from "@/shared/components/input/SearchInput";
-import { BackHeader } from "@/shared/components/header/BackHeader";
-import { Header } from "@/shared/components/header/Header";
-import { useState } from "react";
-import { GroupTab } from "@/shared/components/tab/GroupTab";
-
 /**
  *@description 로그인 페이지
  */
@@ -21,40 +12,7 @@ function LoginPage() {
         justifyContent: "space-between",
         gap: 32,
       }}
-    >
-      <Header />
-
-      <InputField
-        label="아이디"
-        name="email"
-        placeholder="example.com"
-        type="email"
-        errorMessage="error"
-        successMessage="success"
-      />
-
-      <ActiveButton disabled>test</ActiveButton>
-
-      <DropdownButton name="test" />
-
-      <SearchInput />
-
-      <BackHeader
-        title={"회원정보 수정"}
-        onBack={function (): void {
-          throw new Error("Function not implemented.");
-        }}
-      />
-
-      <GroupTab
-        tabs={[
-          { key: "home", label: "홈" },
-          { key: "board", label: "게시판" },
-        ]}
-        activeKey={"home"}
-        onChange={() => {}}
-      />
-    </div>
+    ></div>
   );
 }
 
