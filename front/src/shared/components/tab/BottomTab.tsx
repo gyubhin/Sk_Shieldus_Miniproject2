7e425534b05d3ec4f1fc47230aaf56a27e8b9e43
yunjongyun <yunyami0605@gmail.com>
2025-09-09 10:08:02 +0900

    refactor: 하단 탭 컴포넌트 이벤트 로직 변경
    1. 하단 이벤트 로직 변경

--- front/src/shared/components/tab/BottomTab.tsx
+++ front/src/shared/components/tab/BottomTab.tsx
@@ -1,25 +1,27 @@
 import styles from "./BottomTab.module.scss";
 import clsx from "clsx";
 import { IconButton } from "../icon/IconButton";
+import { useState } from "react";
 
 type ActveKey = "home" | "group" | "mypage";
-
-type Props = {
-  activeKey: ActveKey;
-  onChange: (key: ActveKey) => void;
-};
 
 /**
  *@description 하단 탭 컴포넌트
  *@param actvieKey 활성화된 탭 키
  */
-export function BottomTab({ activeKey, onChange }: Props) {
+export function BottomTab() {
+  const [activeKey, setActvieKey] = useState<ActveKey>();
+
+  const onTabClick = (_key: ActveKey) => {
+    setActvieKey(_key);
+  };
+
   return (
     <nav className={styles.tab_bar}>
       <button
         key={"home"}
         className={clsx(styles.tab_item, activeKey === "home" && styles.active)}
-        onClick={() => onChange("home")}
+        onClick={() => onTabClick("home")}
       >
         <IconButton
           size={20}
@@ -32,7 +34,7 @@ export function BottomTab({ activeKey, onChange }: Props) {
       <button
         key={"group"}
         className={clsx(styles.tab_item, activeKey === "group" && styles.active)}
-        onClick={() => onChange("group")}
+        onClick={() => onTabClick("group")}
       >
         <IconButton
           size={20}
@@ -45,7 +47,7 @@ export function BottomTab({ activeKey, onChange }: Props) {
       <button
         key={"mypage"}
         className={clsx(styles.tab_item, activeKey === "mypage" && styles.active)}
-        onClick={() => onChange("mypage")}
+        onClick={() => onTabClick("mypage")}
       >
         <IconButton
           size={20}
