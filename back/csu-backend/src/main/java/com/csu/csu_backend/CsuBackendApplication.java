6aeed0d99a2531c9fc8e6d6d65c50514547cf306
gyubhin <iami0115@naver.com>
2025-09-09 16:02:21 +0900

    feat: 정모 및 참석자 기능 구현 - 테스트 미완

--- back/csu-backend/src/main/java/com/csu/csu_backend/CsuBackendApplication.java
+++ back/csu-backend/src/main/java/com/csu/csu_backend/CsuBackendApplication.java
@@ -13,9 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class CsuBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CsuBackendApplication.class, args);
 	}
-
 }
