e52801b46cdce84fb3ea631459aeaa14b8cc78cc
yunjongyun <yunyami0605@gmail.com>
2025-09-11 08:50:44 +0900

    design: 공통 컴포넌트 ui 수정 (tag, input, dropdown)
    1. labeledDropdown ui 추가
    2. tag > 삭제 props 및 ui 추가

--- front/src/shared/components/tag/Tag.tsx
+++ front/src/shared/components/tag/Tag.tsx
@@ -2,13 +2,19 @@ import styles from "./Tag.module.scss";
 
 type Props = {
   name: string;
+  onDelete?: () => void;
 };
 
 /**
  *@description 태그 공통 컴포넌트
  */
-function Tag({ name }: Props) {
-  return <span className={styles.tag}>#{name}</span>;
+function Tag({ name, onDelete }: Props) {
+  return (
+    <span className={styles.tag} onClick={onDelete}>
+      #{name}
+      {onDelete && <span className={styles.delete_button}>x</span>}
+    </span>
+  );
 }
 
 export default Tag;
