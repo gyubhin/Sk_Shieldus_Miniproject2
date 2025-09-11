3376d2c7c8b5ba22193b4a99ec0706ae38cc53e3
yunjongyun <yunyami0605@gmail.com>
2025-09-12 08:21:13 +0900

    refactor: 게시글 등록/수정 api 및 기능 수정 (form)
    1. schema, type 수정

--- front/src/features/post/_types/body.ts
+++ front/src/features/post/_types/body.ts
@@ -1,12 +1,12 @@
 /**
- *@description 게시글 생성 요청 바디
+ *@description 게시글 생성 요청 폼 타입
  */
-export type CreatePostBody = {
+export type CreatePostForm = {
   title: string;
   content: string;
 };
 
 /**
- *@description 게시글 수정 요청 바디
+ *@description 게시글 수정 요청 폼 타입
  */
-export type PutPostBody = CreatePostBody;
+export type PutPostForm = CreatePostForm;
