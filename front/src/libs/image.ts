495beb9dcaab943dd547af393af9ede4f3fd1209
yunjongyun <yunyami0605@gmail.com>
2025-09-15 16:00:37 +0900

    refactor: util 수정 (getImageUrl)
    1. origin 그대로 올경우에 대한 예외처리 추가

--- front/src/libs/image.ts
+++ front/src/libs/image.ts
@@ -1,5 +1,7 @@
 export const getImageUrl = (url?: string | null) => {
   if (!url) return undefined;
 
+  if (url.indexOf("http") !== -1) return url;
+
   return `${import.meta.env.VITE_APP_IMG_BASE_URL}${url}`;
 };
